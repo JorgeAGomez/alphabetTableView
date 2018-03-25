313cef5c6d8789608c2624d04f14d671692cfc6d
Jorge Gomez <JorgeG@macminis-Mini.home>
2018-03-25 09:28:04 -0600

    first commit

---
+++ CustomSectionAlphabet/AlphabetView.swift
@@ -0,0 +1,9 @@
+//
+//  AlphabetView.swift
+//  CustomSectionAlphabet
+//
+//  Created by Jorge Gomez on 2018-03-25.
+//  Copyright © 2018 codeByMe. All rights reserved.
+//
+
+import Foundation
---
+++ CustomSectionAlphabet/HeaderView.swift
@@ -0,0 +1,21 @@
+//
+//  HeaderView.swift
+//  CustomSectionAlphabet
+//
+//  Created by Jorge Gomez on 2018-03-25.
+//  Copyright © 2018 codeByMe. All rights reserved.
+//
+
+import UIKit
+
+class HeaderView: UITableViewHeaderFooterView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}

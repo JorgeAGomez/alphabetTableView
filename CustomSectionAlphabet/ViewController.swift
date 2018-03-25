098a61e3bd752239fc2fcb5d8e8d68ef208a220f
macmini <JorgeG@macminis-mini.home>
2018-03-25 05:17:34 -0600

    Initial Commit

---
+++ CustomSectionAlphabet/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  CustomSectionAlphabet
+//
+//  Created by Jorge Gomez on 2018-03-25.
+//  Copyright © 2018 codeByMe. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+
+}
+
